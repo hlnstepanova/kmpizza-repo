87d3630629325395b7813b97eb9e5a641c5e5743
Lena Stepanova <hlnstepanova@yandex.ru>
2022-07-02 18:02:27 +0200

    fix logging

--- backend/src/main/java/dev/tutorial/kmpizza/backend/Main.kt
+++ backend/src/main/java/dev/tutorial/kmpizza/backend/Main.kt
@@ -2,15 +2,16 @@ package dev.tutorial.kmpizza.backend
 
 import dev.tutorial.kmpizza.backend.di.getKoinModule
 import dev.tutorial.kmpizza.backend.storage.exposed.LocalSource
-import io.ktor.serialization.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
+import org.slf4j.event.Level
 
 internal fun Application.module() {
     install(Koin) {
@@ -27,6 +28,10 @@ internal fun Application.module() {
         exception<Throwable> { call, cause ->
             call.respond(cause.toString())
         }
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
     }
 
     install(ContentNegotiation) {
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val KOTLIN_VERSION = "1.6.10"
-    const val KTOR_VERSION = "2.0.1"
+    const val KTOR_VERSION = "2.0.3"
     const val SHADOW_JAR_VERSION = "7.1.1"
     const val EXPOSED_VERSION = "0.36.2"
     const val POSTGRESQL_VERSION = "42.3.1"
@@ -13,6 +13,7 @@ object Versions {
     const val COMPOSE_NAV = "2.5.0-alpha03"
     const val MATERIAL = "1.5.0"
     const val COIL = "2.0.0-rc03"
+    const val SLF4J_VERSION = "2.14.1"
 
     object Jvm {
         const val KTOR_AUTH = "io.ktor:ktor-server-auth:$KTOR_VERSION"
@@ -21,7 +22,9 @@ object Versions {
         const val KTOR_SERVER_NETTY = "io.ktor:ktor-server-netty:$KTOR_VERSION"
         const val KTOR_SERIALIZATION = "io.ktor:ktor-serialization-kotlinx-json:$KTOR_VERSION"
         const val KTOR_SERVER_CONTENT_NEGOTIATION = "io.ktor:ktor-server-content-negotiation:$KTOR_VERSION"
+        const val KTOR_SERVER_CALL_LOGGING = "io.ktor:ktor-server-call-logging:$KTOR_VERSION"
         const val KTOR_STATUS_PAGES = "io.ktor:ktor-server-status-pages:$KTOR_VERSION"
+        const val SLF4J = "org.apache.logging.log4j:log4j-slf4j-impl:$SLF4J_VERSION"
         const val JETBRAINS_EXPOSED_CORE = "org.jetbrains.exposed:exposed-core:$EXPOSED_VERSION"
         const val JETBRAINS_EXPOSED_DAO = "org.jetbrains.exposed:exposed-dao:$EXPOSED_VERSION"
         const val JETBRAINS_EXPOSED_JDBC = "org.jetbrains.exposed:exposed-jdbc:$EXPOSED_VERSION"
