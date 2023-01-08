6cc397642ef0120a347a8980cf5225efdf998550
Lena Stepanova <hlnstepanova@yandex.ru>
2023-01-08 16:23:15 +0100

    Step 29: Add assets and a placeholder for no internet connection

--- iosApp/Pods/Target Support Files/Pods-KMPizza/Pods-KMPizza-umbrella.h
+++ iosApp/Pods/Target Support Files/Pods-KMPizza/Pods-KMPizza-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_iosAppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_iosAppVersionString[];
+FOUNDATION_EXPORT double Pods_KMPizzaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_KMPizzaVersionString[];
 
